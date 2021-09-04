ca218387ad5f945284607a969b167ff791962639
Ho2 <howard_bb@hotmail.com>
2021-09-04 13:52:01 +0800

    update: modification of db connection
    Changed the connection in conn.php and connpdo.php. Modified sql script.

--- conn.php
+++ conn.php
@@ -1,5 +1,16 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "foodordering", "3308");
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+//$con = mysqli_connect("localhost", "root", "", "foodordering", "3308");
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 //check connection
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySql" . mysqli_connect_errno();
--- connpdo.php
+++ connpdo.php
@@ -1,6 +1,16 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost:3308;dbname=foodordering", "root", "");
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+
+    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = new PDO("mysql:host=" . $cleardb_server . ";dbname=" . $cleardb_db, $cleardb_username, $cleardb_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
